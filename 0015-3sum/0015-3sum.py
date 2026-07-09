7512e1892868dd52057a53a0c84cf560193c99d2
Daniel Catonga Tecla <94944243+danielctecla@users.noreply.github.com>
2026-07-09 11:36:52 +0530

    Time: 498 ms (87.92%), Space: 22.4 MB (18.45%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,43 +5,32 @@ class Solution:
         n = len(nums)
         
         for i in range(n - 2):
+            if nums[i] > 0: 
+                break
+            
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
+            
             left = i + 1
             right = n - 1
             
             while left < right:
-                result = nums[i] + nums[left] + nums[right]
-
-                if result == 0:
-                    lastI = nums[i]
+                total = nums[i] + nums[left] + nums[right]
+                
+                if total == 0:
                     triplets.append([nums[i],nums[left],nums[right]])
                     
                     left += 1
                     while left < right and nums[left] == nums[left - 1]:
                         left += 1
+                    
                     right-= 1
                     while left < right and nums[right] == nums[right + 1]:
                         right -= 1
 
-                elif result > 0:
+                elif total > 0:
                     right -= 1
                 else:
                     left += 1
 
         return triplets
-
-
-# after found a triplet, order it and then check if it's in the ans
-
-# [-1,0,1,2,-1,-4]
-# [-4,-1,-1,0,1,2]
-# i = 0 
-# -1
-
-# j = i + 1 -> j = 1
-# 0
-
-# k = j + 1  -> k = 2
-# 1
-#if found pass to the next number (break this loop)
