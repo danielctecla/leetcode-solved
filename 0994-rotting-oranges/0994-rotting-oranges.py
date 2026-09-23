2488b10a8a11d39920cae37e179f25d74a5bc642
Daniel Catonga Tecla <94944243+danielctecla@users.noreply.github.com>
2026-09-22 18:26:10 -0600

    Time: 3 ms (75.53%), Space: 19.2 MB (96.53%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,35 +1,71 @@
-from collections import deque
-
 class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
+    # def orangesRotting(self, grid: list[list[int]]) -> int:
+        
+# grid n x m
+# 0 x 0? no
+# no rotten orange, and not fresh orange?
 
-        r, c = len(grid), len(grid[0])
-        rotOranges = deque()
-        oranges = 0
-        time = 0
+# n x m
+# grid = [[2,1,1],[1,1,0],[0,1,1]]
+# I travel the matrix looking for the rotten orange
+# [[0][0]] save the rotten orange in a stack
+# START LOOP
+#   check the 4-adjacent orange of rotten oranges.
+#   convert to rotten orange and add to the stack
+#   BFS - Stack
+#   for each iteration in the entire loop +1 the counter time
+# finally I return the time of the rotten orange.
+    
+    def orangesRotting(self, grid: list[list[int]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+        queue = []
+        total_time = -1
+        fresh_orange = 0
 
-        for row in range(r):
-            for column in range(c):
-                if grid[row][column] == 1:
-                    oranges += 1
-                elif grid[row][column] == 2:
-                    rotOranges.append((row,column))
-
-        if oranges == 0:
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 2:
+                    queue.append((i,j))
+                elif grid[i][j] == 1:
+                    fresh_orange += 1
+        
+        if fresh_orange == 0:
             return 0
+        
 
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        while queue:
+            current_size_stack = len(queue)
+            total_time += 1
 
-        while rotOranges:
-            for i in range(len(rotOranges)):
-                row, col = rotOranges.popleft()
-                for dr, dc in directions:
-                    nr, nc = row + dr, col + dc
-                    if 0 <= nr < r and 0 <= nc < c and grid[nr][nc] == 1:
-                        rotOranges.append((nr,nc))
-                        grid[nr][nc] = 2
-                        oranges -= 1
-            time += 1
+            for _ in range(current_size_stack):
+                x,y = queue.pop(0)
+                
+                directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-        return time - 1 if oranges == 0 else -1
+                for row,column in directions:
+                    new_row = x + row
+                    new_column = y + column
 
+                    if 0 <= new_row < n and 0 <= new_column < m:
+                        
+                        if grid[new_row][new_column] == 1:
+                            queue.append((new_row, new_column))
+                            grid[new_row][new_column] = 2
+                            fresh_orange -= 1
+
+        return total_time if fresh_orange == 0 else -1
+
+
+            
+# grid = [
+#[2,2,1],
+#[2,1,0],
+#[0,1,1]]
+
+# total_time = 2
+# stack = []
+
+# first loop - double for  O(nxm)
+
+#stack = [(0,1),(1,0)]
